9531354295c801e61f545d6d4ff3c7a76fe01c3a
Notargg <gabriel.graton@estudante.ufscar.br>
2024-06-20 14:59:59 -0300

    Att Audio
    - Feedback Sonoro quando alterar
    - Arurmar Algumas Transições

--- Responda/Assets/SetVolume.cs
+++ Responda/Assets/SetVolume.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 public class SetVolume : MonoBehaviour
 {
     public Slider MusicaFundo;
+
+    public AudioSource MusicaFundoAudio;
     public Slider Efeitos;
+    public AudioSource EfeitosAudio;
     public Slider LeituraTexto;
+
+    public AudioSource LeituraTextoAudio;
+
 
     public Text TextoVoltar;
 
@@ -28,14 +34,30 @@ public class SetVolume : MonoBehaviour
     }
 
     public void AtualizarMusicaFundo(){
+
+        if(MusicaFundo.value != Informacoes.GetValueMusicaFundo()){
         Informacoes.SetValueMusicaFundo(MusicaFundo.value);
+        MusicaFundoAudio.volume = MusicaFundo.value;
+        MusicaFundoAudio.Play();
+        }
+
     }
 
     public void AtualizarEfeitos(){
+        
+        if(Efeitos.value != Informacoes.GetValueEfeitos()){
         Informacoes.SetValueEfeitos(Efeitos.value);
+        EfeitosAudio.volume = Efeitos.value;
+        EfeitosAudio.Play();
+        }
     }
 
     public void AtualizarLeituraTexto(){
+            
+        if(LeituraTexto.value != Informacoes.GetValueLeituraTexto()){
         Informacoes.SetValueLeituraTexto(LeituraTexto.value);
+        LeituraTextoAudio.volume = LeituraTexto.value;
+        LeituraTextoAudio.Play();
+        }
     }
 }
